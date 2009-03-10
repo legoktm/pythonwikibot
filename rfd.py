64cc5ab45de62fff7bf98a9c12e307e2e4d7c077
legoktm.wikipedia <legoktm.wikipedia@e6df2af4-0b61-11de-b8f3-b9fb9eb71b04>
2009-03-10 23:07:01 +0000

    add [[WP:RFD]] tagging script
    git-svn-id: http://pythonwikibot.googlecode.com/svn/trunk@17 e6df2af4-0b61-11de-b8f3-b9fb9eb71b04

---
+++ rfd.py
@@ -0,0 +1,26 @@
+#!usr/bin/python
+#
+# (C) Legoktm, 2009 MIT License 
+#
+"""
+Tags pages for RFD
+Usage: python rfd.py
+"""
+import re, time
+
+from pywikibot import wiki
+o = wiki.Page('User:Arthur Rubin/Roman redirects')
+list = re.findall('\[\[:M(.*?)\]\]', o.get())
+newlist = []
+for i in list:
+	newlist.append(wiki.Page('M' + i))
+append='{{rfd}}\n'
+summary='Tagging for [[WP:RFD]] per [[WP:BOTR#Roman_redirects|request]]'
+for b in newlist:
+	oldtext = b.get(force=True)
+	if 'rfd' in oldtext:
+		print 'Skipping: ' + b.title()
+		pass
+	else:
+		wiki.showDiff(oldtext, append+oldtext)
+		b.put(append+oldtext, summary)
