3525f5172c723b43c6c678ac6e014194a380eb67
legoktm.wikipedia <legoktm.wikipedia@e6df2af4-0b61-11de-b8f3-b9fb9eb71b04>
2009-04-01 03:59:48 +0000

    Add two new scripts
    git-svn-id: http://pythonwikibot.googlecode.com/svn/trunk@46 e6df2af4-0b61-11de-b8f3-b9fb9eb71b04

---
+++ scripts/vitalarticles.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+#
+# (C) Legoktm 2008-2009
+# 
+import re, time
+from pywikibot import wiki, pagegen
+
+def do_page(page, text):
+	try:
+		print unicode(page)
+		try:
+			pagetext = page.get()
+		except wiki.IsRedirectPage, e: #wtf...
+			return text
+		talktext = page.toggletalk().get()
+		#get all of the classes and actions
+		if re.search('class=FA',talktext,re.I):
+			if not re.search('class=fail',talktext,re.I):
+				pgclass = ['FA', 'FA']
+		if re.search('class=A',talktext,re.I):
+			pgclass = ['A', 'A-icon']
+		elif re.search('class=GA',talktext,re.I):
+			pgclass = ['GA', 'GAicon']
+		elif re.search('class=B',talktext,re.I):
+			pgclass = ['B', 'B-icon']
+		elif re.search('class=C',talktext,re.I):
+			pgclass = ['C', 'C-icon']
+		elif re.search('class=stub',talktext,re.I):
+			pgclass = ['Stub', 'Stub-icon']
+		elif re.search('class=start',talktext,re.I):
+			pgclass = ['Start', 'Start-icon']
+		else:
+			print "No class assigned to %s." %str(page)
+			return text
+		print str(page) + " has a class of " + str(pgclass[0])
+		#Check for FGANs, FFAs, and FFACs
+		ffac=re.findall('action(.*?)=FAC', pagetext)
+		ffar=re.findall('action(.*?)=FAR', pagetext)
+		pgclassother = False
+		for i in ffac:
+			if re.search('action%sresult=not promoted' %(i),text,re.I):
+				pgclassother = ['FFAC', '{{FAC-icon}}']
+		if re.search('currentstatus=FGAN',text,re.I):
+			pgclassother = ['FGAN','{{NoGA-icon}}']
+		if re.search('currentstatus=FFA',text,re.I):
+			pgclassother = ['FFA', '{{NoFA}}']
+		for i in ffar:
+			if re.search('action%sresult=demoted' %(i),text,re.I):
+				pgclassother = ['FFAC', '{{FAC-icon}}']
+		if not pgclassother:
+			pgclassother = ['','']
+
+		pgtemp = pgclass[1]
+		pgaltclass = pgclassother[1]
+		#substitute it in.
+		find = u'\{\{%s\}\} \[\[%s\]\]' %(pgtemp, page.title())
+		shouldfind = u'{{%s}} [[%s]]' %(pgtemp, page.title())
+		print find
+#		print wikitext
+		m=re.findall(find, text)
+		try:
+			print m[0]
+			if m[0] == shouldfind:
+				print '[[WP:VA]] does not need updating for %s' %page.aslink()
+				return text
+		except IndexError:
+			print 'Not found, updating [[WP:VA]]'
+		"""Following checks if the article is bolded"""
+		boldcheck=re.findall("'''\[\[%s\]\]'''" %(page.title(regex=True)), text)
+		try:
+			print boldcheck[0]
+			bold=True
+		except IndexError:
+			print 'Not found'
+			bold=False
+		"""Done checking for bolding"""
+		"""Check for howmany # signs, only checks for 3"""
+		numcheck1=re.findall('\n(.*?) (.*?)\[\[%s\]\]' %(page.title(regex=True)), text)
+		print numcheck1
+		try:
+			numcheck=numcheck1[0][0]
+		except IndexError:
+			return text #wtf???
+		print numcheck
+		find2= '(\{\{(.*)\}\}|) \[\[%s\]\]' %(str(page.title(regex=True)))
+		if bold:
+			find3= "%s(.*?)'''\[\[%s\]\]'''" %(numcheck, str(page.title(regex=True)))
+			rep= "%s %s{{%s}} '''[[%s]]'''" %(numcheck, pgaltclass, pgtemp, str(page.title(regex=True)))
+		else:
+			find3= '%s(.*?)\[\[%s\]\]' %(numcheck, str(page.title(regex=True)))
+			rep= '%s %s{{%s}} [[%s]]' %(numcheck, pgaltclass, pgtemp, str(page.title(regex=True)))
+		text = re.sub(find3, rep, text)
+		text = re.sub(find2, shouldfind, text)
+		return text
+	except UnicodeEncodeError:
+		return text
+	except UnicodeDecodeError:
+		return text
+def get_pages():
+	vapage = wiki.Page('Wikipedia:Vital articles')
+	gen = pagegen.links(vapage, ns=0) #we only need mainspace articles
+	text = state0 = vapage.get()
+	num = 0
+	for page in gen:
+		if num < 75:
+			text = do_page(page, text)
+			time.sleep(10)
+		num += 1
+	wiki.showDiff(state0, text)
+	vapage.put(text, 'Bot: Updating Vital Articles')
+if __name__ == '__main__':
+	wiki.setUser('Legobot')
+	get_pages()
---
+++ scripts/welcome.py
@@ -0,0 +1,91 @@
+#!usr/bin/python
+from pywikibot import wiki, userlib
+import re, time, sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+logs = {
+	'commons': 'Project:Welcome log',
+}
+templates = {
+	'commons': '{{subst:welcome}}',
+}
+logupmess = {
+	'commons': 'Bot: Updating log',
+}
+welcomemess = {
+	'commons': 'Welcome!',
+}
+class WelcomeBot:
+		def __init__(self):
+			print 'Initializing...'
+			#---------------------------------------------#
+			#-------------SETTINGS START HERE-------------#
+			#---------------------------------------------#
+			self.editcount = 0						#Must have at least this many edits
+			self.loadusers = 500					#How many users to load
+			self.waittime = 500						#How many seconds to wait after each run
+			self.quitafterrun = False				#Whether to quit after running
+			#---------------------------------------------#
+			#--------------SETTINGS END HERE--------------#
+			#----------DO NOT TOUCH BELOW THIS LINE-------#
+			#---------------------------------------------#
+			self.welcomemess = wiki.translate(welcomemess)
+			self.template = wiki.translate(templates)
+			self.logupmess = wiki.translate(logupmess)
+		def run(self):
+			url = wiki.getWiki().getIndex() + '?title=Special:Log/newusers&limit=' + str(self.loadusers)
+			text = wiki.getURL(url)
+			rec = '\<a href="/w/index\.php\?title=User:(.*?)&amp;action=edit&amp;redlink=1"'
+			list = re.findall(rec, text)
+			userlist = []
+			for i in list:
+				userlist.append(userlib.User(wiki.Page('User:'+i)))
+			logpost = ''
+			for user in userlist:
+				logpost += self.doUser(user)
+				time.sleep(2)
+			self.updatelog(logpost)
+			if not self.quitafterrun:
+				bot = WelcomeBot()
+				bot.run()
+			else:
+				sys.exit()
+		def doUser(self, user):
+			try:
+				usereditcount = user.editcount()
+			except ValueError,e:
+				return ''
+			if usereditcount < self.editcount:
+				print '%s doesn\'t have enough edits to be welcomed.' %user.name()
+				return ''
+			if user.isBlocked():
+				print '%s is blocked.' %user.name()			
+				return '' 
+			if user.Page().toggletalk().exists():
+				print '%s already has a talk page.' %user.name()
+				return ''
+			talkpage = user.Page().toggletalk()
+			print 'Welcoming %s.' %user.name()			
+			talkpage.put(self.template, self.welcomemess)
+			return '\n{{WLE|user=%s|contribs=%s}}' %(user.name(), usereditcount)
+		def updatelog(self, text):
+			rightime = time.localtime(time.time())
+			year = str(rightime[0])
+			month = str(rightime[1])
+			day = str(rightime[2])
+			if len(month) == 1:
+				month = u'0' + month
+			try:
+				self.log = logs[wiki.translate()]
+			except KeyError: #no long on wiki
+				return
+			target = self.log + '/' + year + '/' + month + '/' + day
+			log = wiki.Page(target)
+			if not log.exists():
+				text = '{|border="2" cellpadding="4" cellspacing="0" style="margin: 0.5em 0.5em 0.5em 1em; padding: 0.5em; background: #bfcda5; border: 1px #b6fd2c solid; border-collapse: collapse; font-size: 95%;"\n!User\n!Contribs\n' + text
+			log.put(text, self.logupmess)
+
+if __name__ == '__main__':
+	bot = WelcomeBot()
+	bot.run()
