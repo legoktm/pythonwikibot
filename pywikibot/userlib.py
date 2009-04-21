35b972882aa937bd59973d61ed1da1da99abcc12
legoktm.wikipedia <legoktm.wikipedia@e6df2af4-0b61-11de-b8f3-b9fb9eb71b04>
2009-04-21 23:04:28 +0000

    add a userlib.py file that gives editcount, userpage
    git-svn-id: http://pythonwikibot.googlecode.com/svn/trunk@50 e6df2af4-0b61-11de-b8f3-b9fb9eb71b04

---
+++ pywikibot/userlib.py
@@ -0,0 +1,37 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+import wiki
+__version__ = '$Id$'
+class User:
+	def __init__(self, page):
+		self.page = page
+		self.title = page.title()
+		self.id = page.id()
+#		self.username = self.page.titlewonamespace()
+		self.username = False
+		self.API = wiki.API(wiki=page.getSite())
+	def editcount(self):
+		if not self.username:
+			self.username = self.page.titlewonamespace()
+		params = 'action=query&list=users&ususers=%s&usprop=editcount' %self.username
+		return self.API.query(params)['query']['users'][0]['editcount']
+	def isBlocked(self):
+		if not self.username:
+			self.username = self.page.titlewonamespace()	
+		params = 'action=query&list=users&ususers=%s&usprop=blockinfo' %self.username
+		res = self.API.query(params)['query']['users'][0]
+		if res.has_key('blockedby') or res.has_key('blockreason'):
+			return True
+		return False
+	def Page(self):
+		"""
+		Returns the page object for the userpage
+		"""
+		return self.page
+	def name(self):
+		if not self.username:
+			self.username = self.page.titlewonamespace()
+		"""
+		Returns the users name
+		"""
+		return self.username
