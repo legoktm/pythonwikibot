d892e89ceddfedca2c71526558e16bc2d6c39fe3
legoktm.wikipedia <legoktm.wikipedia@e6df2af4-0b61-11de-b8f3-b9fb9eb71b04>
2009-05-04 21:01:00 +0000

    rename pywikibot/wiki scripts/ have not been updated
    git-svn-id: http://pythonwikibot.googlecode.com/svn/trunk@57 e6df2af4-0b61-11de-b8f3-b9fb9eb71b04

--- pywikibot/__init__.py
+++
@@ -1,3 +0,0 @@
-#!usr/bin/python
-#-*- coding: utf-8 -*-
-__all__ = [ 'catlib', 'config', 'mysql', 'pagegen', 'timedate', 'version', 'wiki']
---
+++ wiki/__init__.py
@@ -0,0 +1,5 @@
+#!usr/bin/python
+#-*- coding: utf-8 -*-
+__all__ = [ 'catlib', 'config', 'mysql', 'pagegen', 'timedate', 'version', 'wiki']
+from wiki import *
+import catlib, mysql, pagegen, timeate, version
